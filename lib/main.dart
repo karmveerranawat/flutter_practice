4667d2f8fc9ddffe51a14708291dc9bf1ce6d0e9
karmveerranawat <karmveerranawat@gmail.com>
2022-01-28 22:41:33 +0530

    using cards and functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
   home: Home(),
@@ -14,60 +15,54 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  List<Quote> quotes = [
+    Quote(quote: 'All is well that ends well', author: 'cyber'),
+    Quote(quote: 'Great powers , great responsibility', author: 'wizard'),
+    Quote(quote: 'Happiness is key to achieving inner peace', author: 'io'),
+  ];
 
+  Widget cards(String quote, String author){
+    return Card(
+      margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              quote,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.amber
+              ),
+            ),
+            Text(
+              author,
+              style: const TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('CyberWizard'),
+        title: const Text('Quotes of the day'),
         centerTitle: true,
         backgroundColor: Colors.redAccent,
       ),
       backgroundColor: Colors.blueGrey,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const SizedBox(height: 20.0,),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-            child: Container(
-              color: Colors.white,
-              child: const Text('1'),
-              padding: const EdgeInsets.only(bottom: 50.0),
-            ),
-          ),
-          const SizedBox(height: 20.0,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: (){},
-                  child: const Text(
-                    '1',
-                  )
-              ),
-              ElevatedButton(
-                  onPressed: (){},
-                  child: const Text(
-                    '2',
-                  )
-              ),
-              ElevatedButton(
-                  onPressed: (){},
-                  child: const Text(
-                    '3',
-                  )
-              ),
-              ElevatedButton(
-                  onPressed: (){},
-                  child: const Icon(
-                      Icons.add
-                  )
-              )
-            ],
-          )
-        ],
+        children: quotes.map((e) => cards(e.quote, e.author)).toList(),
       )
     );
   }
