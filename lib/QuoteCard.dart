da6cca0787724a3d2cc76d74001be09d94b0c72b
karmveerranawat <karmveerranawat@gmail.com>
2022-01-29 22:04:54 +0530

    changes regarding adding del icon and making ui look good

--- lib/QuoteCard.dart
+++ lib/QuoteCard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'quote.dart';
 
@@ -12,25 +13,41 @@ class QuoteCard extends StatelessWidget {
       margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
           children: [
-            Text(
-              quote.quote,
-              style: const TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.amber
+            Expanded(
+              flex: 6,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    quote.quote,
+                    style: const TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.amber
+                    ),
+                  ),
+                  Text(
+                    quote.author,
+                    style: const TextStyle(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green
+                    ),
+                  )
+                ],
               ),
             ),
-            Text(
-              quote.author,
-              style: const TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.green
+            Expanded(
+              flex: 1,
+              child: IconButton(
+                onPressed: (){},
+                icon: const Icon(
+                    Icons.delete_forever
+                ),
               ),
-            )
+            ),
           ],
         ),
       ),
